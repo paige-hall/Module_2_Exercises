1d7d7a4817e81f843f64becc4f2c18f777406815
paige-hall <93939655+paige-hall@users.noreply.github.com>
2022-05-12 00:00:00 -0400

    Add files via upload

---
+++ 01_Intro_to_Databases/exercise/Exercises/23_parks_opened_in_1960s.sql
@@ -0,0 +1,2 @@
+-- 23. The name and date established of parks opened in the 1960s (6 rows)
+select park_name, date_established from park where date_established BETWEEN '1960-01-01' and '1969-12-31';
